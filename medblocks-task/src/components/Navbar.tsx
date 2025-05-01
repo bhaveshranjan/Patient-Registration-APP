5d7f3eb0285b87db4704b10b2a4ce682b380995e
Bhavesh <bhaveshranjanswork@gmail.com>
2025-05-01 18:00:47 +0530

    Add Navbar component with links to Home, Register, and Query

---
+++ medblocks-task/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 flex justify-between">
+      <h1 className="font-bold text-lg">Medblocks Task</h1>
+      <div className="space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/register">Register</Link>
+        <Link href="/query">Query</Link>
+      </div>
+    </nav>
+  )
+}
